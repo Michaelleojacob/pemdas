338ea894b58f3d16ff79399c0c8854b1c7f0ae06
Michael <michaelleojacob@gmail.com>
2021-05-12 13:33:13 -0700

    fixed so many bugs. Fixed a huge bug with the backspace not working correctly. Things were just happening in an order that made no sense, backspace was creating and deleting simutaniously. So I fixed that. Separated creating to getting user input. and deleting on backspace. I believe everything works correctly now, fingers crossed.

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 const enterbtn = document.querySelector('.enterbtn');
 const displayDiv = document.querySelector('.displayExp');
 const displayResult = document.querySelector('.displayResult');
-const numArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+const numArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
 const opArray = ["+", "-", "*", "/", "^"];
-const parensArray = ["(", ")"]
-let expression = '';
+const parensArray = ["(", ")"];
 let userInputArr = [];
 let currNum = '';
+let lastArrItem = userInputArr[userInputArr.length - 1];
+let result = '';
 
+//clears all after a result
+window.addEventListener("keydown", (e) => {
+    if(result === ""){
+        return;
+    }
+    if(result !== ""){
+        userInputArr = [];
+        currNum = '';
+        result = '';
+        displayDiv.textContent = '';
+        displayResult.textContent = '';
+    }
+})
 
-//click on calc
+//click button calc
 enterbtn.addEventListener('click', () => {
     console.log('hello');
 })
 
-//keydown for enter
+//enter
 window.addEventListener('keydown', (e) => {
     if(e.key === "Enter"){
         if(currNum !== ''){
@@ -24,12 +38,23 @@ window.addEventListener('keydown', (e) => {
         if(opArray.includes(userInputArr[userInputArr.length - 1])){
             return;
         }
+
+        checkArrForMultAndDiv();
+        checkArrForAddAndSub();
+
+        let result = userInputArr;
+        displayResult.textContent = result;
+        userInputArr = [];
+        return result;
     }
 })
 
-//keydown for getting the user input
+//numbers - user input
 window.addEventListener('keydown', (e) => {
     if(numArray.includes(e.key)){
+
+        checkCurrNumIsSetLastItemOfArr();
+
         if(currNum.includes(".") && e.key === '.'){
             return;
         }
@@ -38,20 +63,49 @@ window.addEventListener('keydown', (e) => {
     }
 })
 
-//checking if keydown contrains an operand (op)
+//sets currNum as last item in Arr. (userInputArr)
+const checkCurrNumIsSetLastItemOfArr = () => {
+    console.log(userInputArr[userInputArr.length - 1]);
+    if(userInputArr.length > 0){
+        if(userInputArr[userInputArr.length - 1] === "*"){
+            return;
+        }
+        else if(userInputArr[userInputArr.length - 1] === "/"){
+            return;
+        }
+        else if(userInputArr[userInputArr.length - 1] === "+"){
+            return;
+        }
+        else if(userInputArr[userInputArr.length - 1] === "-"){
+            return;
+        }
+        else if(userInputArr[userInputArr.length - 1] === "^"){
+            return;
+        }
+        else if(userInputArr[userInputArr.length - 1] === ""){
+            return;
+        }
+        currNum = userInputArr.pop();
+    }
+}
+
+//operand (op) - user input
 window.addEventListener('keydown', (e) => {
     if(opArray.includes(e.key)){
+        currNumCheckIfEmpty();
+        if(userInputArr.length === 0){
+            return;
+        }
         if(userInputArr[userInputArr.length -1] === ("+" || "-" || "*" || "/" || "^" || "(")){
             return;
         }
-        userInputArr.push(currNum);
         userInputArr.push(e.key)
-        currNum = '';
         displayDiv.textContent += ` ${e.key} `;
+        currNum = '';
     }
 })
 
-//checking if keydown includes parens ( )
+//parens ( ) - user input
 window.addEventListener('keydown', (e) => {
     if(parensArray.includes(e.key)){
         userInputArr.push(e.key);
@@ -59,23 +113,45 @@ window.addEventListener('keydown', (e) => {
     }
 })
 
-//backspace functionality
+//backspace 
 window.addEventListener('keydown', (e) => {
-    if(e.key === "Backspace"){
+    if(e.key === "Backspace"){    
+        
+        if(currNum === ""){
+            if(typeof(userInputArr[userInputArr.length - 1]) === "undefined"){
+                return;
+            }
 
-        userInputArr.pop()
-
-        if(displayDiv.textContent.endsWith(" ")){
-            displayDiv.textContent = displayDiv.textContent.slice(0, -3);
-            return;
-        };
-
-        displayDiv.textContent = displayDiv.textContent.slice(0, -1);
+            if(displayDiv.textContent.endsWith(" ")){
+                displayDiv.textContent = displayDiv.textContent.slice(0, -3);
+                userInputArr.pop();
+                return
+            };
+            
+            displayDiv.textContent = displayDiv.textContent.slice(0, -1);
+    
+            if(userInputArr[userInputArr.length - 1].length === 1){
+                userInputArr.pop();
+                return;
+            }
+    
+            if(userInputArr[userInputArr.length - 1].length > 1){
+                userInputArr[userInputArr.length - 1] = userInputArr[userInputArr.length - 1].slice(0, -1);
+                return;
+            }
+        }
+        else{
+            currNum = currNum.slice(0,-1);
+            displayDiv.textContent = displayDiv.textContent.slice(0,-1);
+        }
+        
     }
 })
 
 //logic for a op b
 const compute = (num1, op, num2) => {
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
     if(op === "+"){
         result = num1 + num2;
         return result;
@@ -171,8 +247,20 @@ const checkArrForAddAndSub = () => {
 //stricktly for testing purposes.
 window.addEventListener('keydown', (e) => {
     console.log(userInputArr);
+    console.log(currNum);
 })
 
+//name
+const currNumCheckIfEmpty = () => {
+    if(currNum !== ''){
+        userInputArr.push(currNum);
+        currNum = '';
+    }
+}
+
+// userInputArr = ["1", "+", "(", "", "3", "*", "4"];
 // userInputArr = ["1", "+", "30", "/", "3", "*", "4"];
+// displayDiv.textContent = userInputArr;
 // userInputArr = ["1", "*", "2", "*", "3"];
-console.log(userInputArr);
+// console.log(userInputArr);
+
